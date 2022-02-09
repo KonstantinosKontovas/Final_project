dab9cceb81c9a6eff51ed255eadc01f3c1dd530d
Konstantinos Kontovas <81429288+KonstantinosKontovas@users.noreply.github.com>
2022-02-09 16:24:51 +0100

    less import

--- model_deploy.py
+++ model_deploy.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 # import matplotlib.patheffects as path_effects
-from matplotlib.pyplot import figure
+# from matplotlib.pyplot import figure
 import plotly.express as px
 
 
